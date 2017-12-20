3fcf361563d525f466931a3bc2aecf5469e8c801
Karol Ciba <karol.ciba@gmail.com>
2017-12-20 21:34:05 +0100

    Initial environment

--- misp.cpp
+++ misp.cpp
@@ -12,8 +12,8 @@ using std::shared_ptr;
 using std::weak_ptr;
 using std::make_shared;
 
-// int LEVEL = 5;
-int LEVEL = 0;
+int LEVEL = 5;
+// int LEVEL = 0;
 int ERROR = 1;
 int WARN  = 2;
 int INFO  = 3;
@@ -64,12 +64,14 @@ std::ostream &operator<<(std::ostream &os, unique_ptr<Token> t) {
 	return os << "Token(" << t->type << "," << t->value << ")";
 }
 
+class Env;
+
 class Type {
 public:
 	virtual void print() {
 		std::cout << "[base]";
 	};
-	virtual unique_ptr<Type> eval(Type* args=nullptr) {
+	virtual unique_ptr<Type> eval(Env &env, Type* args=nullptr) {
 		trace("Type#eval\n");
 		return nullptr;
 	}
@@ -82,7 +84,7 @@ public:
 	void print() {
 		std::cout << "[Atom " << value << "]";
 	}
-	unique_ptr<Type> eval(Type *args=nullptr) {
+	unique_ptr<Type> eval(Env &env, Type *args=nullptr) {
 		trace("AtomType#eval\n");
 		return unique_ptr<Type>(new AtomType(value));
 	}
@@ -91,24 +93,22 @@ public:
 	}
 };
 
-
 class Env {
 public:
 	shared_ptr<Env> outer;
-	std::unordered_map<std::string, Type*> env;
+	std::unordered_map<std::string, unique_ptr<Type>> env;
 
 	Env();
 
-	void set(std::string key, Type* value);
-	Type* find(std::string key);
+	void set(std::string key, unique_ptr<Type> value);
+	Env* find(std::string key);
+	Type* get(std::string key);
 };
 
 class ListType : public Type {
 public:
 	std::deque<unique_ptr<Type>> data;
-	Env* env;
 	ListType() {
-		env = new Env();
 		trace("Conctruting ListType\n");
 	}
 	void print() {
@@ -118,7 +118,7 @@ public:
 		}
 		std::cout << "]";
 	}
-	unique_ptr<Type> eval(Type *inargs=nullptr) {
+	unique_ptr<Type> eval(Env &env, Type *inargs=nullptr) {
 		trace("ListType#eval\n");
 
 		if (data.size() == 0) {
@@ -137,32 +137,37 @@ public:
 
 		auto args = unique_ptr<ListType>(new ListType());
 		for (auto it = data.begin(); it != data.end(); it++) {
-			args->data.push_back( (*it)->eval() );
+			args->data.push_back( (*it)->eval(env) );
 		}
 
 		if (aop->value == "+") {
-			Type* fn = env->find("+");
-			return fn->eval(args.get());
+			trace("ListType#eval plus operator\n");
+			Type* fn = env.get("+");
+			return fn->eval(env, args.get());
+		} else if (aop->value == "def!") {
+			trace("ListType#eval def operator\n");
+			Type* fn = env.get("def!");
+			return fn->eval(env, args.get());
 		}
 		trace("ListType#eval got evaluated params\n");
 
-		return std::move(aop->eval(args.get()));
+		return std::move(aop->eval(env, args.get()));
 	}
 };
 
 class FunctionType : public Type {
 public:
 	Env* env;
-	FunctionType(Env* ienv) : env(ienv) { }
-	unique_ptr<Type> eval(Type* args) {
+	FunctionType() : env() { }
+	unique_ptr<Type> eval(Type* args, Env* env) {
 		trace("FunctionType#eval\n");
 	}
 };
 
 class PlusFunctionType : public FunctionType {
 public:
-	PlusFunctionType(Env *ienv) : FunctionType(ienv) { }
-	unique_ptr<Type> eval(Type* oargs) {
+	PlusFunctionType() : FunctionType() { }
+	unique_ptr<Type> eval(Env &env, Type* oargs) {
 		trace("PlusFunctionType#eval\n");
 		ListType* args = dynamic_cast<ListType*>(oargs);
 		int value = 0;
@@ -173,22 +178,57 @@ public:
 	}
 };
 
+class DefFunctionType : public FunctionType {
+public:
+	DefFunctionType() : FunctionType() { }
+	unique_ptr<Type> eval(Env &env, Type* oargs) {
+		trace("DefFunctionType#eval\n");
+		ListType* args = dynamic_cast<ListType*>(oargs);
+
+		unique_ptr<Type> oname = std::move(*args->data.begin());
+		// unique_ptr<Type> eop = op->eval();
+		AtomType* name = dynamic_cast<AtomType*>(oname.release());
+		if (!name) {
+			std::cout << "SYNTAX ERROR" << "\n";
+		}
+		args->data.pop_front();
+
+		unique_ptr<Type> value = args->eval(env);
+
+		env.set(name->value, std::move(unique_ptr<AtomType>(name)));
+
+		return unique_ptr<Type>(std::move(value));
+	}
+};
 
 Env::Env() {
-	env["+"] = new PlusFunctionType(this);
-	env["-"] = new PlusFunctionType(this);
+    env["+"] = std::move(std::make_unique<PlusFunctionType>());
+	env["-"] = std::move(std::make_unique<PlusFunctionType>());
+	env["def!"] = std::move(std::make_unique<DefFunctionType>());
 }
 
-void Env::set(std::string key, Type* value) {
-	env[key] = value;
+void Env::set(std::string key, unique_ptr<Type> value) {
+	env[key] = std::move(value);
 }
 
-Type* Env::find(std::string key) {
+Env* Env::find(std::string key) {
 	auto it = env.find(key);
 	if (it != env.end()) {
-		return (it->second);
+		return this;
+	} else if (outer) {
+		return outer->find(key);
 	}
+
 	return nullptr;
+}
+
+Type* Env::get(std::string key) {
+	auto it = env.find(key);
+	if (it != env.end()) {
+		return it->second.get();
+	} else {
+		return nullptr;
+	}
 }
 
 class Reader {
@@ -278,7 +318,9 @@ public:
 		}
 	}
 	unique_ptr<ListType> read_list() {
-		auto l = unique_ptr<ListType>(new ListType());
+		//auto l = unique_ptr<ListType>(new ListType());
+		//investigate
+		auto l = std::make_unique<ListType>();
 		while (true) {
 			auto& p = peek();
 			trace("read_list peek: " << &p << "\n");
@@ -323,6 +365,7 @@ public:
 
 int main(int argc, char** argv) {
 	Reader reader;
+	unique_ptr<Env> env = std::make_unique<Env>();
 	do {
 		std::cout << "user> ";
 		auto t = reader.read_form();
@@ -331,7 +374,7 @@ int main(int argc, char** argv) {
 			t->print();
 			std::cout << "\n";
 			trace("Eval ast\n");
-			auto ret = t->eval();
+			auto ret = t->eval(*env);
 			ret->print();
 			std::cout << "\n";
 		}
